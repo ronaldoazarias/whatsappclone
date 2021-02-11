e90fdc4da79e83945b9e49acf71ee9f21dde82a1
Ronaldo Azarias <ronaldo.azarias01@gmail.com>
2021-02-11 08:18:37 -0300

    ...

---
+++ src/Api.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth';
+import 'firebase/firebase-firestore';
+
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDbJfID3NWlUSuox-9_1tcfLd5mP59KZmY",
+    authDomain: "whatsappclone-de492.firebaseapp.com",
+    projectId: "whatsappclone-de492",
+    storageBucket: "whatsappclone-de492.appspot.com",
+    messagingSenderId: "24291541119",
+    appId: "1:24291541119:web:e505e8c0a8f9b89498bf4a"
+  };
